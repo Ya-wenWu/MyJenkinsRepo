ed5dc2ade7b56bbf955b44c08a7a0d215578583f
Yawen.Wu <sandy50422@gmail.com>
2019-01-29 00:10:34 +0800

    ..

--- MyJenkinsRepo1/UnitTestJenkinsProject1/UnitTest1.cs
+++ MyJenkinsRepo1/UnitTestJenkinsProject1/UnitTest1.cs
@@ -24,13 +24,17 @@ namespace UnitTestJenkinsProject1
                             $"C:\\temp\\log\\{DateTime.Now.ToString(Iso8601Foramt)}\\debug.log"));
         }
 
-        //[TestMethod]
-        //public void LogFatalTest()
-        //{
-        //    //Logger.Warn("測試嚴重錯誤");
-        //    //Assert.IsTrue(File.Exists($"" +
-        //    //              $"d:\\web\\unit_test_jenkins_log\\{DateTime.Now.ToString(Iso8601Foramt)}\\fatal.log"));
+        [TestMethod]
+        public void LogFatalTest()
+        {
+            //Logger.Warn("測試嚴重錯誤");
+            //Assert.IsTrue(File.Exists($"" +
+            //              $"d:\\web\\unit_test_jenkins_log\\{DateTime.Now.ToString(Iso8601Foramt)}\\fatal.log"));
+            Logger.Warn("測試偵錯");
+            Assert.IsTrue(File.Exists($"" +
+                            $"C:\\temp\\log\\{DateTime.Now.ToString(Iso8601Foramt)}\\debug.log"));
 
-        //}
+
+        }
     }
 }
